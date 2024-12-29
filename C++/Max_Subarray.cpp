20b14bb1ed82d1af3a340e39d6e4bc20181df2c2
Admin <harut5213@gmail.com>
2024-12-29 16:38:32 +0400

    Max Subarray

---
+++ C++/Max_Subarray.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <vector>
+#include <climits>
+int NlogN(std::vector<int> &nums, int l, int r)
+{
+    if (l == r)
+    {
+        return nums[l] > 0 ? nums[l] : 0;
+    }
+
+    int m = l + (r - l) / 2;
+
+    int left = NlogN(nums, l, m);
+    int right = NlogN(nums, m + 1, r);
+
+    int cross_sum = 0;
+    int l_cross = INT_MIN, r_cross = INT_MIN;
+
+    int temp_sum = 0;
+    for (int i = m; i >= l; --i)
+    {
+        temp_sum += nums[i];
+        l_cross = std::max(l_cross, temp_sum);
+    }
+
+    temp_sum = 0;
+    for (int i = m + 1; i <= r; ++i)
+    {
+        temp_sum += nums[i];
+        r_cross = std::max(r_cross, temp_sum);
+    }
+
+    cross_sum = l_cross + r_cross;
+
+    return std::max({left, right, cross_sum});
+}
+
+int N(std::vector<int> &nums){
+    int n = nums.size();
+       int cur_sum = 0;
+       int max_sum = INT_MIN;
+       for(int i = 0; i < n; i++){
+        cur_sum += nums[i];
+        max_sum = std::max(cur_sum, max_sum);
+        if(cur_sum < 0){
+            cur_sum = 0;
+        }
+       }
+       return max_sum;
+}
--- C++/Simple_Sorting.cpp
+++ C++/Simple_Sorting.cpp
@@ -12,12 +12,16 @@ void bubbleSort(std::vector<int>& arr) {
     }
 }
 
-void selectionSort(std::vector<int>& arr) {
+void selectionSort(std::vector<int> &arr)
+{
     int n = arr.size();
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i)
+    {
         int minIndex = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (arr[j] < arr[minIndex]) {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (arr[j] < arr[minIndex])
+            {
                 minIndex = j;
             }
         }
@@ -25,16 +29,21 @@ void selectionSort(std::vector<int>& arr) {
     }
 }
 
-void insertionSort(std::vector<int>& arr) {
+void insertionSort(std::vector<int> &arr)
+{
     int n = arr.size();
-    for (int i = 1; i < n; ++i) {
+    for (int i = 1; i < n; ++i)
+    {
         int key = arr[i];
         int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             --j;
         }
         arr[j + 1] = key;
     }
 }
+
+
